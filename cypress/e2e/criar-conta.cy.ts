a23e5b78ed4b277bf12a0e3990895a485e0de00b
Marcus Augusto <marcusauguustoo@gmail.com>
2026-06-03 18:11:38 -0400

    test(01): implementa cenários de validação da criação de conta

--- cypress/e2e/criar-conta.cy.ts
+++ cypress/e2e/criar-conta.cy.ts
@@ -34,5 +34,132 @@ describe("Criação de conta no sistema", () => {
         cy.get('[data-cy="user-menu"]').should("be.visible"); // Verifica se o menu do usuário está visível, indicando que o login foi bem-sucedido
       });
     });
+
+    // BUG 4 - E-mail com formato inválido
+    it("Não deve permitir avançar com formato de e-mail inválido (sem @ ou domínio)", () => {
+      cy.get(".css-j9tmj0").click();
+      cy.fixture("criar-conta").then((dados) => {
+        cy.get('[data-cy="nome"]').type(dados.nome);
+        cy.get('[data-cy="dataNascimento"]').type(dados.dataNascimento);
+        cy.get('[data-cy="open-sexo"]').click();
+        cy.get('[data-cy="' + toCyString(dados.sexo) + '"]').click();
+        cy.get('[data-cy="documento"]').type(dados.cpf);
+        cy.get(".css-kh7nmy").click(); 
+        
+        cy.get('[data-cy="email"]').type("email_invalido"); // email inválido
+        cy.get('[data-cy="senha"]').type(dados.senha);
+        cy.get('[data-cy="senhaConfirmar"]').type(dados.senhaConfirmar);
+        cy.get(".css-kh7nmy").click(); 
+        
+        // Verifica que o avanço foi bloqueado e o termo de aceite não apareceu
+        cy.get(".css-d2d35v").should("not.exist");
+      });
+    });
+
+    it("Não deve permitir avançar com senhas divergentes", () => {
+      cy.get(".css-j9tmj0").click();
+      cy.fixture("criar-conta").then((dados) => {
+        cy.get('[data-cy="nome"]').type(dados.nome);
+        cy.get('[data-cy="dataNascimento"]').type(dados.dataNascimento);
+        cy.get('[data-cy="open-sexo"]').click();
+        cy.get('[data-cy="' + toCyString(dados.sexo) + '"]').click();
+        cy.get('[data-cy="documento"]').type(dados.cpf);
+        cy.get(".css-kh7nmy").click();
+        
+        cy.get('[data-cy="email"]').type(dados.email);
+        cy.get('[data-cy="senha"]').type(dados.senha);
+        cy.get('[data-cy="senhaConfirmar"]').type("SenhaErrada123!");
+        cy.get(".css-kh7nmy").click(); 
+        
+        cy.get(".css-d2d35v").should("not.exist");
+      });
+    });
+
+    it("Deve exibir erro ao tentar criar conta com e-mail já cadastrado", () => {
+      cy.get(".css-j9tmj0").click();
+      cy.fixture("criar-conta").then((dados) => {
+        cy.get('[data-cy="nome"]').type(dados.nome);
+        cy.get('[data-cy="dataNascimento"]').type(dados.dataNascimento);
+        cy.get('[data-cy="open-sexo"]').click();
+        cy.get('[data-cy="' + toCyString(dados.sexo) + '"]').click();
+        cy.get('[data-cy="documento"]').type(dados.cpf); 
+        cy.get(".css-kh7nmy").click(); 
+        
+        cy.get('[data-cy="email"]').type(dados.email);
+        cy.get('[data-cy="senha"]').type(dados.senha);
+        cy.get('[data-cy="senhaConfirmar"]').type(dados.senhaConfirmar);
+        cy.get(".css-kh7nmy").click(); 
+        
+        cy.get(".css-d2d35v").click();
+        cy.get('[data-cy="finalizar"]').click();
+        
+        // O modal abre, então validamos que o botão Confirmar aparece
+        cy.contains('button', 'Confirmar').should('be.visible');
+        
+        // AQUI ESTÁ O BUG 5: O teste deve procurar pela justificativa do erro na tela (ex: "já cadastrado").
+        // Como o sistema está exibindo apenas a letra "V" no modal, este passo vai falhar propositalmente, mantendo o Cypress vermelho.
+        cy.contains(/já cadastrado/i).should('be.visible');
+      });
+    });
+
+    it("Não deve permitir avançar na primeira etapa com CPF inválido", () => {
+      cy.get(".css-j9tmj0").click();
+      cy.fixture("criar-conta").then((dados) => {
+        cy.get('[data-cy="nome"]').type(dados.nome);
+        cy.get('[data-cy="dataNascimento"]').type(dados.dataNascimento);
+        cy.get('[data-cy="open-sexo"]').click();
+        cy.get('[data-cy="' + toCyString(dados.sexo) + '"]').click();
+        cy.get('[data-cy="documento"]').type("11111111111"); // CPF matematicamente inválido
+        cy.get(".css-kh7nmy").click(); 
+        
+        // Deve ser impedido de ir para a etapa de e-mail e senha
+        cy.get('[data-cy="email"]').should("not.exist");
+      });
+    });
+
+    it("Não deve permitir avançar com senha fraca", () => {
+      cy.get(".css-j9tmj0").click();
+      cy.fixture("criar-conta").then((dados) => {
+        cy.get('[data-cy="nome"]').type(dados.nome);
+        cy.get('[data-cy="dataNascimento"]').type(dados.dataNascimento);
+        cy.get('[data-cy="open-sexo"]').click();
+        cy.get('[data-cy="' + toCyString(dados.sexo) + '"]').click();
+        cy.get('[data-cy="documento"]').type(dados.cpf);
+        cy.get(".css-kh7nmy").click();
+        
+        cy.get('[data-cy="email"]').type(dados.email);
+        cy.get('[data-cy="senha"]').type("123"); // senha fraca
+        cy.get('[data-cy="senhaConfirmar"]').type("123");
+        cy.get(".css-kh7nmy").click(); 
+        
+        cy.get(".css-d2d35v").should("not.exist");
+      });
+    });
+
+    it("Deve manter o botão Finalizar desabilitado até o aceite dos termos", () => {
+      cy.get(".css-j9tmj0").click();
+      cy.fixture("criar-conta").then((dados) => {
+        cy.get('[data-cy="nome"]').type(dados.nome);
+        cy.get('[data-cy="dataNascimento"]').type(dados.dataNascimento);
+        cy.get('[data-cy="open-sexo"]').click();
+        cy.get('[data-cy="' + toCyString(dados.sexo) + '"]').click();
+        cy.get('[data-cy="documento"]').type(dados.cpf);
+        cy.get(".css-kh7nmy").click();
+        
+        cy.get('[data-cy="email"]').type(dados.email);
+        cy.get('[data-cy="senha"]').type(dados.senha);
+        cy.get('[data-cy="senhaConfirmar"]').type(dados.senhaConfirmar);
+        cy.get(".css-kh7nmy").click(); 
+        
+        // Verifica se o botão finalizar está desabilitado ANTES do aceite
+        cy.get('[data-cy="finalizar"]').should("be.disabled");
+        
+        // Clica no aceite
+        cy.get(".css-d2d35v").click();
+        
+        // Verifica se habilitou (se os atributos da aplicação suportarem)
+        cy.get('[data-cy="finalizar"]').should("not.be.disabled");
+      });
+    });
   });
 });
